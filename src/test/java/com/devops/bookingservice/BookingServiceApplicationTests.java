b96d76eadc7787d4fc322fd829a1240e952fbda5
isidora-stanic <isidora.stanic@outlook.com>
2023-07-24 14:31:25 +0200

    feat: added mongodb which runs as a container

--- src/test/java/com/devops/bookingservice/BookingServiceApplicationTests.java
+++ src/test/java/com/devops/bookingservice/BookingServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.devops.bookingservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestBookingServiceApplication.class)
 class BookingServiceApplicationTests {
 
 	@Test
