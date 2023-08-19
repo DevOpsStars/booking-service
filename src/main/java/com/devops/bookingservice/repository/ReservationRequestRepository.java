781fc058bcfa6c8776abe11ff486301e897cdf2c
isidora-stanic <isidora.stanic@outlook.com>
2023-08-19 17:03:48 +0200

    refactor: changed date type from Date to LocalDate

--- src/main/java/com/devops/bookingservice/repository/ReservationRequestRepository.java
+++ src/main/java/com/devops/bookingservice/repository/ReservationRequestRepository.java
@@ -4,12 +4,12 @@ import com.devops.bookingservice.model.ReservationRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ReservationRequestRepository extends MongoRepository<ReservationRequest, String> {
 
     @Query("{'reservationStart' : { $gte: ?0, $lte: ?1 }, 'reservationEnd' : {$gte: ?0, $lte: ?1}, 'status': 'ACCEPTED' }")
-    List<ReservationRequest> findAcceptedByPeriod(Date start, Date end);
+    List<ReservationRequest> findAcceptedByPeriod(LocalDate start, LocalDate end);
 
 }
